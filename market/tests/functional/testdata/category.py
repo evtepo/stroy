b38b9cce200548e665160428895e131224a1f75d
evtepo <andrey_laguta00@mail.ru>
2024-06-15 16:39:58 +1000

    тестовые данные

---
+++ market/tests/functional/testdata/category.py
@@ -0,0 +1,4 @@
+category1 = {"name": "Cloth"}
+category2 = {"name": "Furniture"}
+
+category2_new_name = {"name": "Cloth", "new_name": "Old clothes"}
---
+++ market/tests/functional/testdata/product.py
@@ -0,0 +1,8 @@
+product = {
+    "name": "Chair",
+    "price": 1000.00,
+    "description": "Some info",
+    "category": "Furniture",
+}
+
+new_price = 1500.00
