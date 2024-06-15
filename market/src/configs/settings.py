75b470c20bf41ee0e34e7bbdf585be985f305079
evtepo <andrey_laguta00@mail.ru>
2024-06-15 16:44:22 +1000

    настройки проекта

---
+++ market/src/configs/settings.py
@@ -0,0 +1,32 @@
+import logging
+
+from dotenv import load_dotenv
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    project_name: str = Field(alias="PROJECT_NAME", default="Stroy-Otdelka")
+
+    localhost: str = Field(alias="LOCALHOST", default="localhost")
+    localport: int = Field(alias="LOCALPORT", default=8000)
+
+    postgres_db: str = Field(alias="POSTGRES_DB", default="otdelka")
+    postgres_user: str = Field(alias="POSTGRES_USER", default="postgres")
+    postgres_password: str = Field(alias="POSTGRES_PASSWORD", default="tanosi99")
+    postgres_host: str = Field(alias="POSTGRES_HOST", default="localhost")
+    postgres_port: str = Field(alias="POSTGRES_PORT", default="5432")
+
+
+settings = Settings()
+
+dsn = f"postgresql+asyncpg://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_host}:{settings.postgres_port}/{settings.postgres_db}"
+
+console = logging.StreamHandler()
+logging.basicConfig(
+    handlers=(console,),
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s: %(message)s",
+)
